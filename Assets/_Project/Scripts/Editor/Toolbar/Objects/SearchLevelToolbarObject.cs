c621ac7d97f0ac72a6b997e7a91d07d096c7c55c
GonzaEsca <gonzalo.escamilla@7r1ck.com>
2024-05-26 17:59:01 -0300

    feat: search level toolbar windows added

---
+++ Assets/_Project/Scripts/Editor/Toolbar/Objects/SearchLevelToolbarObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using _Project.Scripts.Editor.Tools;
+using UnityEditor;
+using UnityEngine;
+
+namespace Pyros.Editor.Toolbar.Objects
+{
+    [Serializable]
+    public class SearchLevelToolbarObject : IToolbarObject
+    {
+        private GUIContent _content;
+
+        public void Setup()
+        {
+            _content = ToolbarGUIContentFactory.CreateContentWithBuiltInEditorTexture("Search On Icon",
+                "Open Search Levels Window", "Search level ");
+        }
+
+        public void Draw()
+        {
+            using(new EditorGUI.DisabledScope(Application.isPlaying))
+            {
+                EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+
+                if(GUILayout.Button(_content, EditorStyles.toolbarButton, GUILayout.Width(110.0f)))
+                {
+                    SearchLevelsWindow.ShowWindow();
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Editor/Tools/SearchLevelsWindow.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.Editor.Tools
+{
+    public class SearchLevelsWindow : EditorWindow
+    {
+        [MenuItem("Tools/Stumble Guys/Levels/Search levels")]
+        public static void ShowWindow()
+        {
+            var window = EditorWindow.GetWindow<SearchLevelsWindow>() ?? EditorWindow.CreateInstance<SearchLevelsWindow>();
+            window.titleContent = new GUIContent("Search levels");
+            window.minSize = window.maxSize = new Vector2(500, 600);
+            window.Show();
+        }
+        private Vector2 scrollPosition;
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Scenes In Build", EditorStyles.boldLabel);
+
+            // Get the scenes in build settings
+            EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+            if (scenes.Length == 0)
+            {
+                GUILayout.Label("No scenes in build settings.");
+                return;
+            }
+
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
+            foreach (EditorBuildSettingsScene scene in scenes)
+            {
+                if (scene == null)
+                    continue;
+
+                string scenePath = scene.path;
+                string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+
+                GUILayout.BeginHorizontal();
+
+                GUILayout.Label(sceneName, GUILayout.Width(200));
+
+                if (GUILayout.Button("Open Scene"))
+                {
+                    OpenScene(scenePath);
+                }
+
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+
+        private void OpenScene(string scenePath)
+        {
+            // Check if the scene is currently open
+            if (SceneManager.GetActiveScene().path != scenePath)
+            {
+                // Save current scene if it has unsaved changes
+                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                {
+                    EditorSceneManager.OpenScene(scenePath);
+                }
+            }
+            else
+            {
+                Debug.Log("Scene is already open.");
+            }
+        }
+    }
+}
